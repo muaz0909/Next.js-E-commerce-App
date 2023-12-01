4872ea3ce075a627b0bada57dce27e5933a6d1a0
Muaz <muazhumayun0@gmail.com>
2023-12-01 15:05:01 +0500

    Update index.tsx

--- components/checkout/index.tsx
+++ components/checkout/index.tsx
@@ -2,22 +2,33 @@ import Grid from "components/grid";
 import Footer from "components/layout/footer";
 import { getCart } from "lib/commerce";
 import { cookies } from "next/headers";
-import { Suspense } from "react";
+import React, { useEffect, useState } from "react";
 import CheckoutForm from "./form";
 import CheckoutSummery from "./summery";
+import {CommerceCart} from "../../lib/commerce/types";
 
-export default async function Checkout() {
-    const cartId = cookies().get('cartId')?.value;
-    const cart = await getCart(cartId || "");
+const Checkout = () => {
+    const [cart, setCart] = useState<CommerceCart | null>(null);
+
+    useEffect(() => {
+        const fetchCart = async () => {
+            const cartId = cookies().get('cartId')?.value;
+            const fetchedCart = await getCart(cartId || "");
+            setCart(fetchedCart);
+        };
+
+        fetchCart();
+    }, []);
+
     return (
         <>
             <Grid className="mx-5 grid-cols-1 sm:grid-cols-1 lg:grid-cols-2">
                 <CheckoutForm />
                 <CheckoutSummery cart={cart} />
             </Grid>
-            <Suspense>
-                <Footer />
-            </Suspense>
+            <Footer />
         </>
-    )
-}
+    );
+};
+
+export default Checkout;
