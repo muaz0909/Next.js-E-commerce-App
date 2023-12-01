b3866c8f518e3e65358a84015c7e03a69b287876
Muaz <muazhumayun0@gmail.com>
2023-12-01 15:21:49 +0500

    update

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,21 +1,14 @@
 import Checkout from 'components/checkout';
 import { getCart } from 'lib/commerce';
 import { cookies } from 'next/headers';
-import React from 'react'; // Import React
-
-// Assuming CommerceCart is a type/interface representing the cart structure
-import type { CommerceCart } from 'lib/commerce/types';
 
 export const runtime = 'edge';
-
-// Define a type for the props expected by the Checkout component
-interface CheckoutProps {
-    cart: CommerceCart | null;
-}
 
 export default async function CheckoutPage() {
     const cartId = cookies().get('cartId')?.value;
     const cart = await getCart(cartId || "");
 
-    return <Checkout cart={cart} />;
+    return (
+        <Checkout cart={cart} />
+    );
 }
